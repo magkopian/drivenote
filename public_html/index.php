d4f37b936b1bc9b81e4b937070edd33f093d4f0c
magkopian <m.agkopian@gmail.com>
2014-10-19 20:13:15 +0300

    Added design

--- public_html/index.php
+++ public_html/index.php
@@ -4,30 +4,41 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Drivenote | Teipir</title>
+	<link rel="stylesheet" type="text/css" href="/css/basestyle.css">
 </head>
 <body>
-	<?php $notification = Notifier::pop(); ?>
-
-	<?php if ( !empty($notification) ): ?>
-		<div class="notification">
-			<span class="notification-title"><?php echo $notification['title']; ?></span>
-			<span class="notification-body"><?php echo $notification['message']; ?></span>
+	<div id="wrapper">
+		<header>
+			<h1 id="title">Drivenote | Teipir</h1>
+		</header>
+		<div id="content">
+			<?php $notification = Notifier::pop(); ?>
+		
+			<?php if ( !empty($notification) ): ?>
+				<div class="notification <?php echo $notification['type']; ?>">
+					<h4><?php echo $notification['title']; ?></h4>
+					<p><?php echo $notification['message']; ?></p>
+				</div>
+			<?php endif; ?>	
+		
+			<?php if ( $user->isSignedIn() === false ): ?>
+				<a class="button" href="<?php echo $auth->getAuthURL(); ?>" title="Click to Sign in"><span>Sign in with Google</span></a>
+			<?php else: ?>
+				<?php if ( $user->isVerified() === false ): ?>
+					<form action="activate-account.php" method="post">
+						<label for="academic_emal">Academic Email:</label>
+						<input type="text" name="academic_email" id="academic_email" placeholder="e.g. cse12345@stef.teipir.gr">
+						<input class="button" type="submit" value="Send Verification">
+					</form>
+				<?php else: ?>
+					<a class="button" href="<?php echo $drive->getFileURL(DIRECTORY_ID); ?>" title="Click to open google drive folder"><span>Open Google Drive</span></a>
+				<?php endif; ?>
+				<a class="button" href="signout.php" title="Click to Sign out"><span>Sign out</span></a>
+			<?php endif; ?>
 		</div>
-	<?php endif; ?>
-	
-	<?php if ( $user->isSignedIn() === false ): ?>
-		<a href="<?php echo $auth->getAuthURL(); ?>" title="Click to Sign in">Sign in with Google</a>
-	<?php else: ?>
-		<a href="signout.php" title="Click to Sign out">Sign out</a>
-		<?php if ( $user->isVerified() === false ): ?>
-			<form action="activate-account.php" method="post">
-				<label for="academic_emal">Academic Email:</label>
-				<input type="text" name="academic_email" id="academic_email">
-				<input type="submit" value="Send Verification">
-			</form>
-		<?php else: ?>
-			<a href="<?php echo $drive->getFileURL(DIRECTORY_ID); ?>" title="Click to open google drive folder">Open Google Drive</a>
-		<?php endif; ?>
-	<?php endif; ?>
+		<footer>
+		
+		</footer>
+	</div>
 </body>
 </html>
