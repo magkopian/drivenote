f4d85b1e41ae3d0d3f62d80565acac618e048106
magkopian <m.agkopian@gmail.com>
2014-10-21 00:44:44 +0300

    Made layout more responsive

--- public_html/index.php
+++ public_html/index.php
@@ -9,6 +9,7 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Drivenote | <?php echo SCHOOL_NAME; ?></title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0;">
 	<link href='http://fonts.googleapis.com/css?family=Cantarell' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" type="text/css" href="/css/basestyle.css">
 	<!--[if IE]><link rel="shortcut icon" href="favicon.ico"><![endif]-->
